1916a674f1b656396bfcbde32c9dccaa41817789
Edgar Rupare <erupar200@caledonian.ac.uk>
2019-04-28 15:58:21 +0100

    Initial commit

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,31 @@
+// strings
+console.log('hello, world');
+
+let email = 'example@me.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Mickey';
+let lastName = 'Mouse'
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+let result = email.lastIndexOf('m');
+
+console.log(result);
+
